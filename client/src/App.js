c758277b845d483daa30a881aa32ee29b428ab55
kezu1003 <keshani2001karunarathne@gmail.com>
2026-01-10 15:52:32 +0530

    updated node modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import SearchPage from './pages/SearchPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import MyLibraryPage from './pages/MyLibraryPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import './App.css';
 
 function App() {
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>React App is Working! 🎉</h1>
-      <p>If you see this, React is running correctly.</p>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/" element={<SearchPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              
+              {/* Protected Routes */}
+              <Route 
+                path="/my-library" 
+                element={
+                  <ProtectedRoute>
+                    <MyLibraryPage />
+                  </ProtectedRoute>
+                } 
+              />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
 export default App;
+
+
+
